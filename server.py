756d502a9a78fc3ea9918382b8a7ef44baadf19b
Kelly Zhou <kelzhou3@gmail.com>
2015-01-17 17:33:33 -0500

    layout changes

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 import sqlite3
-# from Ziggeo import Ziggeo
+from Ziggeo import Ziggeo
 from flask import Flask, render_template, request, g, redirect, url_for, \
              abort, flash, session
 from contextlib import closing
@@ -8,7 +8,7 @@ import datetime
 app = Flask(__name__, static_folder='static', static_url_path='')
 app.config.from_object('__init__')
 
-# ziggeo = Ziggeo("61113f2c9913e0daecefe88965a37dac", "d6e6a6620c4ae5f36c3a47dfac6fc274", "11cd57eafb2262c7829983d76bcff0c6")
+ziggeo = Ziggeo("61113f2c9913e0daecefe88965a37dac", "d6e6a6620c4ae5f36c3a47dfac6fc274", "11cd57eafb2262c7829983d76bcff0c6")
 
 @app.route("/")
 def index():
