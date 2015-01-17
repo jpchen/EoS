bf38d692945d4d5199397afa50793a1b32665ecb
jpchen <jonathanp.chen@gmail.com>
2015-01-17 03:00:11 -0500

    modified schema

--- schema.sql
+++ schema.sql
@@ -2,7 +2,10 @@ drop table if exists users;
 drop table if exists videos;
 create table users (
   name text not null,
-  hash text not null
+  hash text not null,
+  email text not null,
+  school text not null,
+  prof integer not null
 );
 create table videos {
   id integer primary key,
