b56f0cf16206770b32a2418d36246a2a933fbb5d
Kelly Zhou <kelzhou3@gmail.com>
2015-01-18 03:12:43 -0500

    fixed dashboard styling

--- server.py
+++ server.py
@@ -5,6 +5,7 @@ from flask import Flask, render_template, request, g, redirect, url_for, \
 from contextlib import closing
 import datetime
 import unicodedata
+import time
 
 app = Flask(__name__, static_folder='static', static_url_path='')
 app.config.from_object('__init__')
